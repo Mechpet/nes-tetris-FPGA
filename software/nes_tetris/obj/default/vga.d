c2ded23702d9c3fcb3dd74e831e16f0bc2ab44f8
Mechpet <blazepet2@gmail.com>
2021-11-17 05:23:40 -0600

    Implemented score value, level value, line value

---
+++ software/nes_tetris/obj/default/vga.d
@@ -0,0 +1,10 @@
+obj/default/vga.o: vga.c ../nes_tetris_bsp/system.h \
+ ../nes_tetris_bsp/linker.h ../nes_tetris_bsp//HAL/inc/alt_types.h vga.h
+
+../nes_tetris_bsp/system.h:
+
+../nes_tetris_bsp/linker.h:
+
+../nes_tetris_bsp//HAL/inc/alt_types.h:
+
+vga.h:
--- vga_avl_interface.sv
+++ vga_avl_interface.sv
@@ -2,8 +2,7 @@
 Avalon-MM Interface VGA Text mode display
 
 Register Map:
-0x000-0x0257 : VRAM, 80x30 (2400 byte, 600 word) raster order (first column then row)
-0x258        : control register
+0x000 = [31:16] Level number, [15:0] Line count
 
 VRAM Format:
 X->
@@ -45,124 +44,120 @@ module vga_avl_interface (
 	output logic hs, vs						// VGA HS/VS
 );
 
-logic RAM_WRITE;
-assign RAM_WRITE = AVL_WRITE & ~AVL_ADDR[11];
-
 logic [11:0] AVL_READADDR;
-logic [31:0] RAM_READDATA, PALETTE_READDATA;
 
-assign AVL_READDATA = (AVL_ADDR[11]) ? PALETTE_READDATA : RAM_READDATA;
-
-/*
-byte_enabled_simple_dual_port_ram ram0
-(
- .we(RAM_WRITE), 
- .clk(CLK),
- .waddr(AVL_ADDR[10:0]),
- .raddr(AVL_READADDR),
- .be(AVL_BYTE_EN),
- .wdata(AVL_WRITEDATA),
- .q(RAM_READDATA)
-);*/
 ram ram0
 (.byteena_a(AVL_BYTE_EN),
 	.clock(CLK),
 	.data(AVL_WRITEDATA),
 	.rdaddress(AVL_READADDR),
 	.wraddress(AVL_ADDR[10:0]),
-	.wren(RAM_WRITE),
-	.q(RAM_READDATA));
+	.wren(AVL_WRITE),
+	.q(AVL_READDATA));
 
-logic [31:0] PALETTE [7:0]; // Palette register data
-
-//put other local variables here
-// `onoroff` refers to whether a pixel at (DrawX, DrawY) on the VGA should be background (0) or foreground (1)
-// `inv` refers to whether a pixel at (DrawX, DrawY) was inverted
-// `bit_index` refers to the bit position/index in the ASCII 8-bit data (0 -> bit at data[0])
-// `char_index` refers to the character position/index in the registers (0 -> data at register[7:0])
-// `row` refers to the row of the character to be displayed (0 -> left of VGA)
-// `col` refers to the column of the character to be displayed (0 -> top of VGA)
 logic pixel_clk, blank, sync, onoroff, inv, Increment;
-logic [2:0] bit_index, char_index;
+logic [3:0] bit_index;
 logic [6:0] row, col;
-logic [11:0] reg_index;
 logic [9:0] DrawX, DrawY;
-logic bg_bit, fg_bit;
-logic [3:0] palette_bg_index, palette_fg_index, palette_bg_reg, palette_fg_reg, palette_addr;
-logic [3:0] r, b, g, br, bg, bb, fr, fg, fb;
+logic [3:0] r, b, g;
 
 // Font-related
 logic [9:0] addr;
-logic [7:0] data;
+logic [15:0] data;
 	
 //Declare submodules..e.g. VGA controller, ROMS, etc
 vga_controller VGA_CONTROLLR(.Clk(CLK), .Reset(RESET), .hs(hs), .vs(vs), .pixel_clk(pixel_clk), .blank(blank), .sync(sync), .DrawX(DrawX), .DrawY(DrawY));
-font_rom FONT_ROM(.addr(addr), .data(data));
+font_rom_upscaled FONT_ROM(.addr(addr), .data(data));
 
 assign red = r;
 assign green = g;
 assign blue = b;
 
-// Read and write from AVL interface to register block, note that READ waitstate = 1, so this should be in always_ff
-always_ff @(posedge CLK) begin
-	if (RESET)
-		PALETTE <= '{default:'0};	// Reset all registers to 0
-	else
-	begin	
-	if (AVL_CS & AVL_ADDR[11])	// Chip-select indicates operation
-		begin
-			palette_addr <= AVL_ADDR - 12'h800;
-			// Assume that only one operation can be performed at a given time
-			if (AVL_WRITE)
-			begin
-				unique case (AVL_BYTE_EN)	// Write a subset of the data at register[]
-					4'b1111 : PALETTE[palette_addr] <= AVL_WRITEDATA;
-					4'b1100 : PALETTE[palette_addr][31:16] <= AVL_WRITEDATA[31:16];
-					4'b0011 : PALETTE[palette_addr][15:0] <= AVL_WRITEDATA[15:0];
-					4'b1000 : PALETTE[palette_addr][31:24] <= AVL_WRITEDATA[31:24];
-					4'b0100 : PALETTE[palette_addr][23:16] <= AVL_WRITEDATA[23:16];
-					4'b0010 : PALETTE[palette_addr][15:8] <= AVL_WRITEDATA[15:8];
-					4'b0001 : PALETTE[palette_addr][7:0] <= AVL_WRITEDATA[7:0];
-					default : ;
-				endcase
-			end
-			else if (AVL_READ)
-				PALETTE_READDATA <= PALETTE[palette_addr];
-		end
-	end
-end
-
-always_comb
-begin // Decode electron beam position
-		col = DrawX >> 3;
-		row = DrawY >> 3;
-		bit_index = ~DrawX[2:0];
-		if (row == score_label_row && (col >= score_label_left_col && col <= score_label_right_col))
-		begin //Fetch font data (LUT)
+always_comb begin // Decode electron beam position
+		AVL_READADDR = 0;
+		col = DrawX >> 4;
+		row = DrawY >> 4;
+		bit_index = ~DrawX[3:0];
+		
+		// Static labels look-up table
+		if (row == score_label_row && (col >= score_label_left_col && col <= score_label_right_col)) begin 
 			unique case (col)
-				68 : addr = start_S + DrawY[2:0]; // 'S'
-				69 : addr = start_C + DrawY[2:0]; // 'C'
-				70 : addr = start_O + DrawY[2:0]; // 'O'
-				71 : addr = start_R + DrawY[2:0]; // 'R'
-				72 : addr = start_E + DrawY[2:0]; // 'E'
+				28 : addr = start_S + DrawY[3:0]; // 'S'
+				29 : addr = start_C + DrawY[3:0]; // 'C'
+				30 : addr = start_O + DrawY[3:0]; // 'O'
+				31 : addr = start_R + DrawY[3:0]; // 'R'
+				32 : addr = start_E + DrawY[3:0]; // 'E'
+				default : addr = 45 * 16;
 			endcase
 		end
-		else
-			addr = 9'h2D * 8;
+		else if (row == lines_label_row && (col >= lines_label_left_col && col <= lines_label_right_col)) begin
+			unique case (col)
+				15 : addr = start_L + DrawY[3:0];    // 'L'
+				16 : addr = start_I + DrawY[3:0];    // 'I'
+				17 : addr = start_N + DrawY[3:0];    // 'N'
+				18 : addr = start_E + DrawY[3:0];    // 'E'
+				19 : addr = start_S + DrawY[3:0];    // 'S'
+				20 : addr = start_dash + DrawY[3:0]; // '-'
+				default : addr = 45 * 16;
+			endcase
+		end
+		else if (row == level_label_row && (col >= level_label_left_col && col <= level_label_right_col)) begin
+			unique case (col)
+				28, 32 : addr = start_L + DrawY[3:0]; // 'L'
+				29, 31 : addr = start_E + DrawY[3:0]; // 'E'
+				30 : addr = start_V + DrawY[3:0]; 	  // 'V'
+				default : addr = 45 * 16;
+			endcase
+		end 
+		else if (row == score_val_row && (col >= score_val_left_col && col <= score_val_right_col)) begin
+			AVL_READADDR = score_addr;
+			unique case (col)
+				25 : addr = AVL_READDATA[31:28] * 16 + DrawY[3:0];
+				26 : addr = AVL_READDATA[27:24] * 16 + DrawY[3:0];
+				27 : addr = AVL_READDATA[23:20] * 16 + DrawY[3:0];
+				28 : addr = AVL_READDATA[19:16] * 16 + DrawY[3:0];
+				29 : addr = AVL_READDATA[15:12] * 16 + DrawY[3:0];
+				30 : addr = AVL_READDATA[11:8] * 16 + DrawY[3:0];
+				31 : addr = AVL_READDATA[7:4] * 16 + DrawY[3:0];
+				32 : addr = AVL_READDATA[3:0] * 16 + DrawY[3:0];
+				default : addr = 45 * 16;
+			endcase
+		end
+		else if (row == level_val_row && (col >= level_val_left_col && col <= level_val_right_col)) begin
+			AVL_READADDR = level_lines_addr;
+			unique case (col)
+				29 : addr = AVL_READDATA[31:28] * 16 + DrawY[3:0];
+				30 : addr = AVL_READDATA[27:24] * 16 + DrawY[3:0];
+				31 : addr = AVL_READDATA[23:20] * 16 + DrawY[3:0];
+				32 : addr = AVL_READDATA[19:16] * 16 + DrawY[3:0];
+				default : addr = 45 * 16;
+			endcase
+		end
+		else if (row == lines_val_row && (col >= lines_val_left_col && col <= lines_val_right_col)) begin
+			AVL_READADDR = level_lines_addr;
+			unique case (col)
+				21 : addr = AVL_READDATA[15:12] * 16 + DrawY[3:0];
+				22 : addr = AVL_READDATA[11:8] * 16 + DrawY[3:0];
+				23 : addr = AVL_READDATA[7:4] * 16 + DrawY[3:0];
+				24 : addr = AVL_READDATA[3:0] * 16 + DrawY[3:0];
+				default : addr = 45 * 16;
+			endcase
+		end
+		else begin
+			addr = 45 * 16;
+		end
 		onoroff = data[bit_index];
 end
 
 
 //handle drawing (may either be combinational or sequential - or both).
 always_ff @ (posedge pixel_clk) begin
-	if (RESET || !blank) // If don't need to display, then display black
-	begin
+	if (RESET || !blank) begin
 		r <= 4'h0;
 		g <= 4'h0;
 		b <= 4'h0;
 	end
-	else 	// If need to display, paint the current pixel
-	begin
+	else begin
 		r <= (onoroff) ? 4'b1111 : 4'h0 ;
 		g <= (onoroff) ? 4'b1111 : 4'h0 ;
 		b <= (onoroff) ? 4'b1111 : 4'h0 ;
